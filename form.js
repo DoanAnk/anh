7415a38cd23c18f8502e4cefc3fa2487e13561fc
DoanAnh <108865160+DoanAnk@users.noreply.github.com>
2022-08-05 11:15:45 +0900

    Update form.js

--- form.js
+++ form.js
@@ -1,180 +1,6 @@
+/****************************************************************************************************************************/
 'use strict'
-// function Validator(options){
-
-//     function getParent(element, selector) {
-//         while(element.parentElement){
-//             if(element.parentElement.matches(selector)){
-//                 return element.parentElement;
-//             }
-//             element = element.parentElement;
-//         }
-//     }
-//     var selectorRules = {};
-
-//     // Ham thuc hien validator
-//     function validator(inputElement, rule) {
-//         // var errorElement = getParent(inputElement, '.form-group')
-//         var errorMessage;
-//         var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector)
-
-//         // lấy ra các rule of selector 
-//         var rules = selectorRules[rule.selector];
-
-//         //lặp qua từng rule và kiểm tra
-//         //Nếu có lỗi thì dừng vc ktra
-//         for(var i = 0; i < rules.length; ++i){
-//             switch (inputElement.type) {
-//                 case 'checkbox':
-//                 case 'radio':
-//                     errorMessage = rules[i](
-//                         formElement.querySelector(rule.selector + ':checked')
-//                     );                   
-//                     break;
-//                 default:
-//                     errorMessage = rules[i](inputElement.value);
-//             }
-//             errorMessage = rules[i](inputElement.value);
-//             if (errorMessage) break;
-//         }
-
-//         if(errorMessage) {
-//             errorElement.innerText = errorMessage;
-//             getParent(inputElement, options.formGroupSelector).classList.add('invalid')
-//         } else {
-//             errorElement.innerText = '';
-//             getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
-//         }
-//         return !errorMessage;
-//     }
-//     // Lay element cua form can validator
-//     var formElement = document.querySelector(options.form)
-    
-//     if(formElement){
-//         //Khi submit form
-//         formElement.onsubmit = function(e){
-//             e.preventDefault();
-
-//             var isFormValid = true;
-//             //thuc hiện lặp qua từng rule và validate
-//             options.rules.forEach(function(rule){
-//                 var inputElement = formElement.querySelector(rule.selector);
-//                 var isValid = validator(inputElement, rule);
-//                 if(!isValid){
-//                     isFormValid = false;
-//                 }
-//             });
-
-            
-            
-//             if(isFormValid){
-//                 // trường hợp submit vs js
-//                 if(typeof options.onSubmit === 'function'){
-//                     var enableInputs = formElement.querySelectorAll('[name]')
-//                     var formValues = Array.from(enableInputs).reduce(function(values, input){
-                        
-//                         switch(input.type) {
-//                             case 'checkbox':
-//                             case 'radio':
-//                                 values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value
-//                             break;
-//                             default:
-//                                 values[input.name] = input.value
-
-//                         }
-                        
-                        
-//                         return values;
-//                     },{});
-//                     options.onSubmit(formValues);
-//                 }
-//             }
-//             //trường hợp submit vs hành vi mặc định
-//             else{
-//                 // formElement.submit();
-//                 console.error('ERROR')
-//             }
-//         }
-
-
-//         //lặp qua mỗi rule và xủ lý
-//         options.rules.forEach(function(rule){
-
-//             // lưu lại các rule cho các input
-//             if(Array.isArray(selectorRules[rule.selector])) {
-//                 selectorRules[rule.selector].push(rule.test);
-//             }else{
-//                 selectorRules[rule.selector] = [rule.test]
-//             }
-
-
-//             var inputElement = formElement.querySelector(rule.selector);
-            
-//             // blur ra ngoai thi bao loi
-//             if(inputElement) {
-//                 //xu ly trg hop blur ra khoi input
-//                 inputElement.onblur = function(){
-//                     validator(inputElement, rule);
-//                 }
-
-//                 //xu ly khi ng dung nhap vapo input
-//                 inputElement.oninput = function() {
-//                     var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector)
-//                     errorElement.innerText = '';
-//                     getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
-//                 }
-
-//             }
-//         });
-//     }
-// }
-// // Dinh nghia rules
-// // Nguyen tac cua rules: 
-// //1. Khi co loi => Tra ra mess loi
-// //2. Khi hop le => Khong tra ra cai gi ca (undefined)
-// Validator.isRequired = function(selector, message){
-//     return {
-//         selector: selector,
-//         test: function(value) {
-//             //trim() : loai bo dau cach
-//             return value ? undefined : message || 'Vui long nhap truong nay'
-//         }
-//     };
-// }
-// Validator.isEmail = function(selector, message){
-//     return {
-//         selector: selector,
-//         test: function(value) {
-//             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//             return regex.test(value) ? undefined : message || 'Truong nay phai la email';
-//         }
-//     };
-// }
-// Validator.mineLength = function(selector, min, message){
-//     return {
-//         selector: selector,
-//         test: function(value) {
-//             return value.length >= min ? undefined : message || `Vui long nhap toi thieu ${min} ky tu` ;
-//         }
-//     };
-// }
-// Validator.isConfirmed = function(selector, getConfirmValue, message){
-//     return {
-//         selector: selector,
-//         test: function(value) {
-//             return value === getConfirmValue() ? undefined : message || 'gia tri nhap vao ko chinh xac';
-//         }
-//     };
-// }
-
-
-
-
-
-
-
-
-
-/*************************************************************************************************************************** */
+/****************************************************************************************************************************/
 
 
 function Validator(options){
@@ -196,6 +22,7 @@ function Validator(options){
         
         //lay cac rule of selector
         var rules = selectorRules[rule.selector]
+        
         //lap qua cac rule
         for(var i=0; i< rules.length; i++) {
 
@@ -209,7 +36,6 @@ function Validator(options){
                 default:
                     errorMessage = rules[i](inputElement.value);
             }
-
             if(errorMessage) break;
             
         }
@@ -227,20 +53,14 @@ function Validator(options){
         return !errorMessage;
     }
 
-
-
-
-
-    var formElement = document.querySelector(options.form)
-    
+    var formElement = document.querySelector(options.form) 
     if (formElement) {
+        
         //khi submit form
         formElement.onsubmit = function (e) {
             e.preventDefault();
 
             var isFormValid = true;
-
-
             options.rules.forEach(function (rule) {
                 var inputElement = formElement.querySelector(rule.selector);
                 var isValid = validate(inputElement, rule)
@@ -249,11 +69,12 @@ function Validator(options){
                 }
             });
             if (isFormValid) {
+                
                 //truong hop submit vs js
                 if (typeof options.onSubmit === 'function') {
+                    
                     var enableInputs = formElement.querySelectorAll('[name]');
                     var formValues = Array.from(enableInputs).reduce(function(values, input) {
-                        
                         switch(input.type) {
                             case 'radio':
                             case 'checkbox':
@@ -262,43 +83,41 @@ function Validator(options){
                             default:
                                 values[input.name] = input.value;
                             }
-                        
-                        
                         return values;
                     },{});
                     options.onSubmit(formValues);
                 }
-                //truong hop submit vs hanh vi mac dinh
+                //
                 else{
                     // formElement.submit();
                 }            
             }
         }
 
-        // duyet qua array dunf forEach()
+        //forEach()：繰り返す
         options.rules.forEach(function(rule) {
-            //luu lai cac rule
+            
+            //各rule保存
             if (Array.isArray(selectorRules[rule.selector])){
                 selectorRules[rule.selector].push(rule.test)
             }else{
                 selectorRules[rule.selector] = [rule.test];
             }
 
-
-
-
-
             var inputElements = formElement.querySelectorAll(rule.selector);
-
             Array.from(inputElements).forEach(function(inputElement) {
-                 //xu ly blur input
+                
+                 //ブルーするときに処理
                  inputElement.onblur = function () {
                     validate(inputElement,rule);
                 }
-                //xu ly khi ng dung nhap vao input
+                
+                //入力するときに処理
                 inputElement.oninput = function () {
+                    
                     var errorElement = getParent(inputElement, options.formGroupSelector).querySelector('.form-message');
                     errorElement.innerText = '';
+                    
                     //xoa class cho the cha of n
                     getParent(inputElement, options.formGroupSelector).classList.remove('invalid') 
                 }
@@ -308,16 +127,11 @@ function Validator(options){
 
 };
 
-
-// // Dinh nghia rules
-// // Nguyen tac cua rules: 
-// //1. Khi co loi => Tra ra mess loi
-// //2. Khi hop le => Khong tra ra cai gi ca (undefined)
 Validator.isRequired = function(selector, message) {
     return {
         selector: selector,
         test: function(value) {
-            return value ? undefined : message || 'vui long nhap truong nay';
+            return value ? undefined : message || 'ここに入力してください';
         }
     };
 }
@@ -326,7 +140,7 @@ Validator.isEmail = function(selector, message){
         selector: selector,
         test: function(value) {
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : message || 'Truong nay phai la email';
+            return regex.test(value) ? undefined : message || 'ここにメールアドレスでないといけません';
         }
     };
 }
@@ -334,7 +148,7 @@ Validator.minLength = function(selector, min, message){
     return {
         selector: selector,
         test: function(value) {
-            return value.length >= min ? undefined : message || `vui long nhap toi thieu ${min} ky tu`;
+            return value.length >= min ? undefined : message || `最低 ${min}数字を入力してください`;
         }
     };
 }
@@ -342,7 +156,7 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value === getConfirmValue() ? undefined : message || 'Gia tri nhap vao k dung';
+            return value === getConfirmValue() ? undefined : message || 'パスワード確認を間違っています。';
         }
     }
 }
